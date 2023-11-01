e0ece8415db8c04539653b84db0bbbbb945d4979
Divyam Jain <jain.divyam.271@gmail.com>
2023-11-01 07:48:16 +0530

    homepage UI

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -24,6 +24,86 @@ class _homePageState extends State<homePage> {
         centerTitle: true,
         elevation: 0.0,
       ),
+      body: Column(
+        children: [
+          SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    child: Icon(
+                      Icons.location_pin,
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(
+                    child: Text(
+                      'CITYNAME',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            width: 500,
+            child: Card(
+              elevation: 0.0,
+              child: Text(
+                'Best Hotels in CITYNAME',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 350,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                SizedBox(
+                  width: 400,
+                  child: Card(
+                    child: Text(
+                      'HOTEL IMAGE',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 100),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 400,
+                  child: Card(
+                    child: Text(
+                      'HOTEL IMAGE',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 100),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 400,
+                  child: Card(
+                    child: Text(
+                      'HOTEL IMAGE',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 100),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
